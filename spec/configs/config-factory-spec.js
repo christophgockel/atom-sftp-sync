49c7ea1388dd3e472cd45879211f5c8e5c070335
Christoph Gockel <github@christophgockel.de>
2018-01-26 08:37:34 +0000

    Fixes wrong usage of async specs for ConfigFactory specs

--- spec/configs/config-factory-spec.js
+++ spec/configs/config-factory-spec.js
@@ -1,5 +1,7 @@
 "use babel";
 
+/* global waitsForPromise */
+
 import ConfigFactory from "../../lib/configs/ConfigFactory";
 import ConfigurationFileSyntaxErrorException from "../../lib/exceptions/ConfigurationFileSyntaxErrorException";
 
@@ -22,34 +24,37 @@ describe("ConfigFactory", () => {
     expect(config.port).toEqual(21);
   });
 
-  it("parses a config file content", (done) => {
+  it("parses a config file content", () => {
     const content = `
       {
         "port": 1234
       }
     `;
 
-    factory.parseConfigFile(content).then((connection) => {
+    waitsForPromise(() => factory.parseConfigFile(content).then((connection) => {
       expect(connection.port).toEqual(1234);
-    })
-      .then(done, done);
+    }));
   });
 
-  it("rejects invalid config files", (done) => {
+  it("rejects invalid config files", () => {
     const invalidContent = "invalid-json";
 
-    factory.parseConfigFile(invalidContent).catch((error) => {
-      expect(error instanceof ConfigurationFileSyntaxErrorException).toBeTruthy();
-    })
-      .then(done, done);
+    factory.parseConfigFile(invalidContent)
+      .then(() => {
+        throw new Error("should have been rejected");
+      })
+      .catch((error) => {
+        expect(error instanceof ConfigurationFileSyntaxErrorException).toBeTruthy();
+      });
   });
 
   it("rejects when no config file could be found", () => {
     const errorThatShouldNotHappen = new Error("should have been rejected");
 
-    factory.loadConfig("non-existant-file").then(() => {
-      throw errorThatShouldNotHappen;
-    })
+    factory.loadConfig("non-existant-file")
+      .then(() => {
+        throw errorThatShouldNotHappen;
+      })
       .catch((e) => {
         expect(e).not.toBe(errorThatShouldNotHappen);
       });
